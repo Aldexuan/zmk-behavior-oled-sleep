006fd542f42294d9d0033d5db72a89a9a2d52f8f
Aldebaran <aldebaran@AldebarandeMacBook-Pro.local>
2026-01-01 21:49:17 +0800

    test

--- src/behavior/oled_sleep.c
+++ src/behavior/oled_sleep.c
@@ -4,14 +4,22 @@
  */
 
 #include <zmk/behavior.h>
-#include <drivers/oled.h>
-
-static bool oled_sleep_state = false;
+#include <drivers/display.h>
 
 static int oled_sleep_binding_accept(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
-    oled_set_display(!oled_sleep_state);
-    oled_sleep_state = !oled_sleep_state;
+    const struct device *display = DEVICE_DT_GET(DT_CHOSEN(zmk_display));
+    if (!device_is_ready(display)) {
+        return -ENODEV;
+    }
+    static bool is_on = true;
+    display_blanking_off(display);
+    if (!is_on) {
+        display_blanking_off(display);
+    } else {
+        display_blanking_on(display);
+    }
+    is_on = !is_on;
     return ZMK_BEHAVIOR_ACCEPTED;
 }
 
@@ -19,5 +27,5 @@ static const struct zmk_behavior_driver_api oled_sleep_behavior_driver_api = {
     .binding_accept = oled_sleep_binding_accept,
 };
 
-ZMK_BEHAVIOR_DECLARE(oled_sleep_behavior, "zmk,behavior-oled-sleep",
+ZMK_BEHAVIOR_DECLARE(oled_sleep, "zmk,behavior-oled-sleep",
                      &oled_sleep_behavior_driver_api);
